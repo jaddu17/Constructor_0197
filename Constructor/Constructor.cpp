f24e7746558593c3a260c611eb5fb3c9d0fdc157
jaddu17 <shafajaddu@gmail.com>
2024-05-14 12:24:33 +0700

    menampilkan

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -33,3 +33,18 @@ void Mahasiswa::cetak() {
     cout << endl << "Nim =" << nim << endl;
     cout << "Nama = " << nama << endl;
 }
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Tesa");
+    Mahasiswa mhs4(20, "Tesa");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+    system("Pause");
+    return 0;
+}
