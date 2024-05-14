cda24b43d7215fae0999486f27d581c7148f0fb4
jaddu17 <shafajaddu@gmail.com>
2024-05-14 12:23:37 +0700

    menjalankan fungsi

--- staticfunction/staticfunction.cpp
+++ staticfunction/staticfunction.cpp
@@ -17,3 +17,16 @@ public:
 
     Mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+int Mahasiswa::nim = 197;
+
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID =  " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
