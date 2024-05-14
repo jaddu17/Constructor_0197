c20b8177895716db979c931421335318f32a2316
jaddu17 <shafajaddu@gmail.com>
2024-05-14 12:24:06 +0700

    membuat class mahasiswa

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -18,33 +18,3 @@ public:
     Mahasiswa(int iNim, string iNama);
     void cetak();
 };
-
-Mahasiswa::Mahasiswa(string iNama) {
-    nim = 0;
-    nama = iNama;
-}
-
-Mahasiswa::Mahasiswa(int iNim, string iNama) {
-    nim = iNim;
-    nama = iNama;
-}
-
-void Mahasiswa::cetak() {
-    cout << endl << "Nim =" << nim << endl;
-    cout << "Nama = " << nama << endl;
-}
-
-int main()
-{
-    Mahasiswa mhs1;
-    Mahasiswa mhs2(20);
-    Mahasiswa mhs3("Tesa");
-    Mahasiswa mhs4(20, "Tesa");
-
-    mhs1.cetak();
-    mhs2.cetak();
-    mhs3.cetak();
-    mhs4.cetak();
-    system("Pause");
-    return 0;
-}
