c6cf069a7a20cd7a26e6e234fa398fdc8e240192
jaddu17 <shafajaddu@gmail.com>
2024-05-14 12:24:21 +0700

    memanggil fungsi

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -18,3 +18,18 @@ public:
     Mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+Mahasiswa::Mahasiswa(string iNama) {
+    nim = 0;
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
+void Mahasiswa::cetak() {
+    cout << endl << "Nim =" << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
