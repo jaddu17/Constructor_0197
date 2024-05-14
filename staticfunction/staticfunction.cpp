450f1c0bb82ee77698dcf7b98705b56505a5226e
jaddu17 <shafajaddu@gmail.com>
2024-05-14 12:23:21 +0700

    membuat class mahasiswa

--- staticfunction/staticfunction.cpp
+++ staticfunction/staticfunction.cpp
@@ -17,31 +17,3 @@ public:
 
     Mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
-
-int Mahasiswa::nim = 197;
-
-void Mahasiswa::setID() {
-    id = ++nim;
-}
-
-void Mahasiswa::printAll() {
-    cout << "ID =  " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main() {
-    Mahasiswa mhs1("Sri Dadi");
-    Mahasiswa mhs2("Budi Jatmiko");
-    Mahasiswa::setNim(9);   // mengakses nim melalui static member function "setNim"
-    Mahasiswa mhs3("Andi Janu");
-    Mahasiswa mhs4("Joko Wahono");
-
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-
-    cout << "akses dari luar object = " << Mahasiswa::getNim() << endl;     // mengakses nim
-    system("pause");
-}
